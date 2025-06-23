406ff0e61f845b2a10a2138c398da4906acf8095
ssy1248 <ssy1248@gmail.com>
2025-06-23 22:48:33 +0900

    fix 씬 구성 변경

--- Assets/Scripts/UI/StageSelectPanel.cs
+++ Assets/Scripts/UI/StageSelectPanel.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageSelectPanel : BasePanel
 {
     public override UIPanelType TypeOfPanel => UIPanelType.STAGE_SELECT_PANEL;
+
+    public void SetStageValue(int stageDistance)
+    {
+        // 각 스테이지 거리 세팅
+        GlobalVariable.Instance.CheckPointDistance = stageDistance;
+
+        SceneManager.LoadScene("ShopScene");
+    }
 
     public override void OnEnter(params object[] datas)
     {
