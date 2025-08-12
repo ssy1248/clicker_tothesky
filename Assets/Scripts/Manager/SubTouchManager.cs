e21062b92268ddd067f77ebac2c091bc77258dea
ssy1248 <ssy1248@gmail.com>
2025-08-12 15:25:23 +0900

    feat: 서브터치, 서브터치 매니저 코드 완성

--- Assets/Scripts/Manager/SubTouchManager.cs
+++ Assets/Scripts/Manager/SubTouchManager.cs
@@ -2,5 +2,52 @@ using UnityEngine;
 
 public class SubTouchManager : MonoBehaviour
 {
-    
+    public static SubTouchManager Instance { get; private set; }
+
+    [Header("미니게임 프리팹")]
+    public GameObject quickTapPrefab;
+    public GameObject touchPrefab;
+    public GameObject memoryPrefab;
+
+    [Header("생성 위치")]
+    public Transform spawnParent; // 미니게임이 생성될 부모 (Canvas 등)
+
+    private void Awake()
+    {
+        if (Instance == null) { Instance = this; }
+        else { Destroy(gameObject); }
+    }
+
+    /// <summary>
+    /// 지정된 타입의 미니게임을 생성하고 시작합니다.
+    /// </summary>
+    public void SpawnMiniGame(SubTouchType type, int successScore)
+    {
+        GameObject prefabToSpawn = null;
+        switch (type)
+        {
+            case SubTouchType.QuickTap:
+                prefabToSpawn = quickTapPrefab;
+                break;
+            case SubTouchType.Touch:
+                prefabToSpawn = touchPrefab;
+                break;
+            case SubTouchType.Memory:
+                prefabToSpawn = memoryPrefab;
+                break;
+        }
+
+        if (prefabToSpawn != null)
+        {
+            // 프리팹을 생성하고 부모를 지정
+            GameObject miniGameObj = Instantiate(prefabToSpawn, spawnParent);
+
+            // 생성된 오브젝트의 SubTouch 스크립트를 가져와 초기화
+            SubTouch subTouch = miniGameObj.GetComponent<SubTouch>();
+            if (subTouch != null)
+            {
+                subTouch.Initialize(successScore);
+            }
+        }
+    }
 }
